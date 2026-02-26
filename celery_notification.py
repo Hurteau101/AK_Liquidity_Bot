8ab135337a236f761fa6e57d6f9ee10df93210bf
Devon <devonhurteau@gmail.com>
2026-02-25 18:40:48 -0600

    Celery database confliction

--- celery_notification.py
+++ celery_notification.py
@@ -8,8 +8,8 @@ from ProcessManager import ProcessManager
 
 celery_app = Celery(
     "notify_user_celery",
-    broker="redis://localhost:6379/9",
-    backend="redis://localhost:6379/9",
+    broker="redis://localhost:6379/6",
+    backend="redis://localhost:6379/6",
 )
 
 
