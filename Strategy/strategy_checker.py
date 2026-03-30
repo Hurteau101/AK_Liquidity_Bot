0cea1c97b1bd66bae636bc599d211bb392b8b03e
Devon <DevonHurteau@gmail.com>
2026-03-29 20:51:31 -0600

    Updated strategy_checker as it wasn't storing the keys to prevent repeated pings

--- Strategy/strategy_checker.py
+++ Strategy/strategy_checker.py
@@ -87,8 +87,8 @@ def run_strategy_check():
             if strategy.part_of_strategy(
                 stat_type=liquidity_data.get("stat_type", "").lower(), league=liquidity_data.get("league", "").lower()
             ) and strategy.run_match_analysis(liquidity_context=context_list, strategy_bot_instance=strategy_bot):
+                redis_strategy_sent_instance.set(name=key, ex=int(start_date_dt.timestamp() * 1000), value="")
                 break
 
 
 
-run_strategy_check()
