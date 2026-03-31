e8530b7c677f416a8a65205005ac56969accfce7
Devon <DevonHurteau@gmail.com>
2026-03-31 17:48:39 -0600

    There was a issue of early returning in strategy

--- Strategy/strategy_checker.py
+++ Strategy/strategy_checker.py
@@ -43,7 +43,7 @@ def run_strategy_check():
 
         liquidity_data = json.loads(raw_liquidity_data)
 
-        start_date = liquidity_data.get("start_date", {})
+        start_date = liquidity_data.get("start_date", "")
 
         start_date_dt = datetime.fromisoformat(start_date.replace("Z", "+00:00"))
         modified_date = start_date_dt - timedelta(minutes=9)
@@ -51,44 +51,41 @@ def run_strategy_check():
 
 
         # We only want to check if we are within the 9 minute window before the game starts. If we are outside of that, we skip.
-        if now_utc <= modified_date:
-            continue
-
-
-        matches = db.get_games(
-            game_title=liquidity_data.get("game_title"),
-            game_start_time=start_date_dt,
-            league=liquidity_data.get("league"),
-            stat_type=liquidity_data.get("stat_type")
-        )
-
-        if not matches:
-            return
-
-        context_list = [
-            LiquidityContext(
-                **{
-                    **match.get("liquidity_context", {}),
-                    "strategy": LiquidityStrategy(
-                        snapshot_time=match.get("snapshot_time"),
-                        include_tag=True,
-                    )
-                }
+        if now_utc >= modified_date:
+            matches = db.get_games(
+                game_title=liquidity_data.get("game_title"),
+                game_start_time=start_date_dt,
+                league=liquidity_data.get("league"),
+                stat_type=liquidity_data.get("stat_type")
             )
-            for match in matches
-        ]
 
-        strategy_bot = StrategyBot()
+            if not matches:
+                continue
 
-        league = liquidity_data.get("league", "").upper()
-        mapped_strategy = STRATEGIES.get(league, [])
-        for strategy  in mapped_strategy:
-            print("Checking strategy: ", strategy.__class__.__name__)
-            if strategy.part_of_strategy(
-                stat_type=liquidity_data.get("stat_type", "").lower(), league=liquidity_data.get("league", "").lower()
-            ) and strategy.run_match_analysis(liquidity_context=context_list, strategy_bot_instance=strategy_bot):
-                redis_strategy_sent_instance.set(name=key, ex=int(start_date_dt.timestamp() * 1000), value="")
-                break
+            context_list = [
+                LiquidityContext(
+                    **{
+                        **match.get("liquidity_context", {}),
+                        "strategy": LiquidityStrategy(
+                            snapshot_time=match.get("snapshot_time"),
+                            include_tag=True,
+                        )
+                    }
+                )
+                for match in matches
+            ]
+
+            strategy_bot = StrategyBot()
+
+            league = liquidity_data.get("league", "").upper()
+            mapped_strategy = STRATEGIES.get(league, [])
+            for strategy  in mapped_strategy:
+                print("Checking strategy: ", strategy.__class__.__name__)
+                if strategy.part_of_strategy(
+                    stat_type=liquidity_data.get("stat_type", "").lower(), league=liquidity_data.get("league", "").lower()
+                ) and strategy.run_match_analysis(liquidity_context=context_list, strategy_bot_instance=strategy_bot):
+                    redis_strategy_sent_instance.set(name=key, ex=int(start_date_dt.timestamp() * 1000), value="")
+                    break
 
 
 
